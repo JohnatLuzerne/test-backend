2680ca8ef13214e59601564841653a22430ffaef
JohnatLuzerne <48885060+JohnatLuzerne@users.noreply.github.com>
2026-05-03 20:27:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 
-portals = {}
+conn = sqlite3.connect("game.db", check_same_thread=False)
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS portals (
+    id INTEGER PRIMARY KEY,
+    faction TEXT
+)
+""")
+conn.commit()
 app = FastAPI()
 
 @app.get("/ping")
@@ -11,7 +20,13 @@ def ping():
 def echo(value: str):
     return {"you_sent": value}
 
+
 @app.get("/capture")
 def capture(portal_id: int, faction: str):
-    portals[portal_id] = faction
+    cursor.execute(
+        "INSERT OR REPLACE INTO portals (id, faction) VALUES (?, ?)",
+        (portal_id, faction)
+    )
+    conn.commit()
+
     return {"portal_id": portal_id, "controlled_by": faction}
